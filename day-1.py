332ecd99d48a9295203b25d5b80e1172655ea16c
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-05-10 23:28:40 +0530

    Day-1 Complete.

---
+++ day-1.py
@@ -0,0 +1,14 @@
+# pets_name = input("Please Enter Your Pets Name:")
+# city_name = input("Please Enter Your Dream City:")
+# band_name = pets_name+city_name
+# print(f"Your bands name is {band_name}")
+
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+
+print(f"a:{a}")
+print(f"b:{b}")
+
